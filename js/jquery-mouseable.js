1da312168ee31004142eacfa405811881d7ea8ae
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2015-02-01 13:41:08 -0500

    Updated commenting.

--- js/jquery-mouseable.js
+++ js/jquery-mouseable.js
@@ -1,8 +1,8 @@
 /**
- * jQuery mouseable plugin
+ * jQuery mouseable() plugin
  * 
- * Custom jQuery method that adds hover and active classes to elements
- * JavaScript replacement for CSS :hover and :active pseudo classes and is !DOCTYPE-agnostic
+ * Custom jQuery method that adds hover and active classes to DOM elements...
+ * A !DOCTYPE-agnostic replacement for CSS :hover and :active pseudo classes
  * 
  * Copyright (c) 2015 Dan Zervoudakes
  * Released under the MIT license
